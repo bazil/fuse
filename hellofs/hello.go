eed9ac61b4498e311ae20ee929edffe957f01800
Tommi Virtanen <tv@eagain.net>
2013-02-25 16:40:19 -0800

    Make hellofs take mountpoint on commandline.
    The previous hardcoded value of /mnt is typically only usable by root.

--- hellofs/hello.go
+++ hellofs/hello.go
@@ -6,14 +6,31 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
 	"bazil.org/fuse"
 )
 
+var Usage = func() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "  %s MOUNTPOINT\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
 func main() {
-	c, err := fuse.Mount("/mnt")
+	flag.Usage = Usage
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		Usage()
+		os.Exit(2)
+	}
+	mountpoint := flag.Arg(0)
+
+	c, err := fuse.Mount(mountpoint)
 	if err != nil {
 		log.Fatal(err)
 	}
