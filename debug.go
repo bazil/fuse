2407d66fbc4c3b7895934f6b82088ca7d60bc44f
Nick Cooper <nmvc@google.com>
2015-02-26 10:29:04 +1100

    add detailed debugger interface

--- debug.go
+++ debug.go
@@ -9,13 +9,28 @@ func stack() string {
 	return string(buf[:runtime.Stack(buf, false)])
 }
 
-func nop(msg interface{}) {}
-
-// Debug is called to output debug messages, including protocol
-// traces. The default behavior is to do nothing.
+// Debug is used to output debug messages and protocol traces. The
+// default behaviour is to do nothing.
 //
 // The messages have human-friendly string representations and are
 // safe to marshal to JSON.
 //
 // Implementations must not retain msg.
-var Debug func(msg interface{}) = nop
+type Debugger interface {
+	// Begin begins a trace span, returns a key to close that span.
+	Begin(msg interface{}) interface{}
+
+	// End ends a trace span.
+	End(key, msg interface{})
+
+	// Print outputs a message not from a span.
+	Print(msg interface{})
+}
+
+type nopDebugger struct {}
+
+func (nopDebugger) Begin(msg interface{}) interface{} {}
+func (nopDebugger) End(key, msg interface{}) {}
+func (nopDebugger) Print(msg interface{})
+
+var Debug Debugger = nopDebugger{}
