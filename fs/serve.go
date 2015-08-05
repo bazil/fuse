bazil/fuse

commit b8f1b8c20fd76393a37b6ee4fada664ce408b9a3

API change: Deprecate NodeRef, Node implementations must be comparable
Now all Nodes get the benefit of detecting the same value returned
multiple times, and you don't need to embed NodeRef for that.

When the NodeRef code was added, I don't think I realized how powerful
maps with interface keys could be.

This also makes it potentially safe to mount the same FS instance
twice, as the Nodes no longer contain mount-specific state.

fs/serve.go
@@ -4,7 +4,6 @@ package fs // import "bazil.org/fuse/fs"
 
 import (
 	"encoding/binary"
-	"errors"
 	"fmt"
 	"hash/fnv"
 	"io"
@@ -77,8 +76,17 @@ type FSInodeGenerator interface {
 // See the documentation for type FS for general information
 // pertaining to all methods.
 //
+// A Node must be usable as a map key, that is, it cannot be a
+// function, map or slice.
+//
 // Other FUSE requests can be handled by implementing methods from the
 // Node* interfaces, for example NodeOpener.
+//
+// Methods returning Node should take care to return the same Node
+// when the result is logically the same instance. Without this, each
+// Node will get a new NodeID, causing spurious cache invalidations,
+// extra lookups and aliasing anomalies. This may not matter for a
+// simple, read-only filesystem.
 type Node interface {
 	// Attr fills attr with the standard metadata for the node.
 	Attr(ctx context.Context, attr *fuse.Attr) error
@@ -323,6 +331,7 @@ func New(conn *fuse.Conn, config *Config) *Server {
 	s := &Server{
 		conn:         conn,
 		req:          map[fuse.RequestID]*serveRequest{},
+		nodeRef:      map[Node]fuse.NodeID{},
 		dynamicInode: GenerateDynamicInode,
 	}
 	if config != nil {
@@ -347,6 +356,7 @@ type Server struct {
 	meta       sync.Mutex
 	req        map[fuse.RequestID]*serveRequest
 	node       []*serveNode
+	nodeRef    map[Node]fuse.NodeID
 	handle     []*serveHandle
 	freeNode   []fuse.NodeID
 	freeHandle []fuse.HandleID
@@ -371,14 +381,15 @@ func (s *Server) Serve(fs FS) error {
 	if err != nil {
 		return fmt.Errorf("cannot obtain root node: %v", err)
 	}
-	if nodeRef, ok := root.(nodeRef); ok {
-		// Recognize the root node if it's ever returned from Lookup,
-		// passed to Invalidate, etc.
-		ref := nodeRef.nodeRef()
-		ref.id = 1
-		ref.generation = s.nodeGen
-	}
-	s.node = append(s.node, nil, &serveNode{inode: 1, node: root, refs: 1})
+	// Recognize the root node if it's ever returned from Lookup,
+	// passed to Invalidate, etc.
+	s.nodeRef[root] = 1
+	s.node = append(s.node, nil, &serveNode{
+		inode:      1,
+		generation: s.nodeGen,
+		node:       root,
+		refs:       1,
+	})
 	s.handle = append(s.handle, nil)
 
 	for {
@@ -414,9 +425,20 @@ type serveRequest struct {
 }
 
 type serveNode struct {
-	inode uint64
-	node  Node
-	refs  uint64
+	inode      uint64
+	generation uint64
+	node       Node
+	refs       uint64
+
+	// Delay freeing the NodeID until waitgroup is done. This allows
+	// using the NodeID for short periods of time without holding the
+	// Server.meta lock.
+	//
+	// Rules:
+	//
+	//     - hold Server.meta while calling wg.Add, then unlock
+	//     - do NOT try to reacquire Server.meta
+	wg sync.WaitGroup
 }
 
 func (sn *serveNode) attr(ctx context.Context, attr *fuse.Attr) error {
@@ -433,52 +455,20 @@ type serveHandle struct {
 	nodeID   fuse.NodeID
 }
 
-// NodeRef can be embedded in a Node to recognize the same Node being
-// returned from multiple Lookup, Create etc calls.
-//
-// Without this, each Node will get a new NodeID, causing spurious
-// cache invalidations, extra lookups and aliasing anomalies. This may
-// not matter for a simple, read-only filesystem.
-type NodeRef struct {
-	id         fuse.NodeID
-	generation uint64
-
-	// Delay freeing the NodeID until waitgroup is done. This allows
-	// using the NodeID for short periods of time without holding the
-	// Server.meta lock.
-	//
-	// Rules:
-	//
-	//     - hold Server.meta while calling wg.Add, then unlock
-	//     - do NOT try to reacquire Server.meta
-	wg sync.WaitGroup
-}
-
-// nodeRef is only ever accessed while holding Server.meta
-func (n *NodeRef) nodeRef() *NodeRef {
-	return n
-}
-
-type nodeRef interface {
-	nodeRef() *NodeRef
-}
+// NodeRef is deprecated. It remains here to decrease code churn on
+// FUSE library users. You may remove it from your program now;
+// returning the same Node values are now recognized automatically,
+// without needing NodeRef.
+type NodeRef struct{}
 
 func (c *Server) saveNode(inode uint64, node Node) (id fuse.NodeID, gen uint64) {
 	c.meta.Lock()
 	defer c.meta.Unlock()
 
-	var ref *NodeRef
-	if nodeRef, ok := node.(nodeRef); ok {
-		ref = nodeRef.nodeRef()
-
-		if ref.id != 0 {
-			// dropNode guarantees that NodeRef is zeroed at the same
-			// time as the NodeID is removed from Server.node, as
-			// guarded by c.meta; this means sn cannot be nil here
-			sn := c.node[ref.id]
-			sn.refs++
-			return ref.id, ref.generation
-		}
+	if id, ok := c.nodeRef[node]; ok {
+		sn := c.node[id]
+		sn.refs++
+		return id, sn.generation
 	}
 
 	sn := &serveNode{inode: inode, node: node, refs: 1}
@@ -491,11 +481,8 @@ func (c *Server) saveNode(inode uint64, node Node) (id fuse.NodeID, gen uint64) 
 		id = fuse.NodeID(len(c.node))
 		c.node = append(c.node, sn)
 	}
-	gen = c.nodeGen
-	if ref != nil {
-		ref.id = id
-		ref.generation = gen
-	}
+	sn.generation = c.nodeGen
+	c.nodeRef[node] = id
 	return
 }
 
@@ -547,13 +534,9 @@ func (c *Server) dropNode(id fuse.NodeID, n uint64) (forget bool) {
 
 	snode.refs -= n
 	if snode.refs == 0 {
+		snode.wg.Wait()
 		c.node[id] = nil
-		if nodeRef, ok := snode.node.(nodeRef); ok {
-			ref := nodeRef.nodeRef()
-			ref.wg.Wait()
-			ref.id = 0
-			ref.generation = 0
-		}
+		delete(c.nodeRef, snode.node)
 		c.freeNode = append(c.freeNode, id)
 		return true
 	}
@@ -1434,19 +1417,15 @@ func errstr(err error) string {
 }
 
 func (s *Server) invalidateNode(node Node, off int64, size int64) error {
-	nodeRef, ok := node.(nodeRef)
-	if !ok {
-		return errors.New("for invalidation, node must embed NodeRef")
-	}
-	ref := nodeRef.nodeRef()
-
 	s.meta.Lock()
-	ref.wg.Add(1)
-	id := ref.id
+	id, ok := s.nodeRef[node]
+	if ok {
+		snode := s.node[id]
+		snode.wg.Add(1)
+		defer snode.wg.Done()
+	}
 	s.meta.Unlock()
-	defer ref.wg.Done()
-
-	if id == 0 {
+	if !ok {
 		// This is what the kernel would have said, if we had been
 		// able to send this message; it's not cached.
 		return fuse.ErrNotCached
@@ -1513,19 +1492,15 @@ func (i invalidateEntryDetail) String() string {
 // Returns ErrNotCached if the kernel is not currently caching the
 // node.
 func (s *Server) InvalidateEntry(parent Node, name string) error {
-	nodeRef, ok := parent.(nodeRef)
-	if !ok {
-		return errors.New("for invalidation, node must embed NodeRef")
-	}
-	ref := nodeRef.nodeRef()
-
 	s.meta.Lock()
-	ref.wg.Add(1)
-	id := ref.id
+	id, ok := s.nodeRef[parent]
+	if ok {
+		snode := s.node[id]
+		snode.wg.Add(1)
+		defer snode.wg.Done()
+	}
 	s.meta.Unlock()
-	defer ref.wg.Done()
-
-	if id == 0 {
+	if !ok {
 		// This is what the kernel would have said, if we had been
 		// able to send this message; it's not cached.
 		return fuse.ErrNotCached

commit dad98ae92ea2413a3fb3fc2c242b1b4a4bab9b5e

Fix a bug that caused recycled NodeID generation count to be 0
The bug was introduced in b8f1b8c20fd76393a37b6ee4fada664ce408b9a3
and is yet another example of why naked returns should not be used.

Linux handled this quietly, OSXFUSE gave ESTALE and logged
"OSXFUSE: vnode changed generation".

fs/serve.go
@@ -494,7 +494,7 @@ func (c *Server) saveNode(inode uint64, node Node) (id fuse.NodeID, gen uint64) 
 	}
 	sn.generation = c.nodeGen
 	c.nodeRef[node] = id
-	return
+	return id, sn.generation
 }
 
 func (c *Server) saveHandle(handle Handle, nodeID fuse.NodeID) (id fuse.HandleID) {