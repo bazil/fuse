eb684f305798564fa747a3ae1d6bb3dd164d465e
Tommi Virtanen <tv@eagain.net>
2019-12-20 12:38:05 -0700

    Use spawntest for BenchmarkLookup

--- fs/bench/bench_lookup_test.go
+++ fs/bench/bench_lookup_test.go
@@ -2,12 +2,14 @@ package bench_test
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 	"bazil.org/fuse/fs/fstestutil"
+	"bazil.org/fuse/fs/fstestutil/spawntest/httpjson"
 )
 
 type benchLookupDir struct {
@@ -20,20 +22,43 @@ func (f *benchLookupDir) Lookup(ctx context.Context, req *fuse.LookupRequest, re
 	return nil, fuse.ENOENT
 }
 
+type benchLookupRequest struct {
+	Path string
+	N    int
+}
+
+func doBenchLookup(ctx context.Context, req benchLookupRequest) (*struct{}, error) {
+	for i := 0; i < req.N; i++ {
+		if _, err := os.Stat(req.Path); !os.IsNotExist(err) {
+			return nil, fmt.Errorf("Stat: wrong error: %v", err)
+		}
+	}
+	return &struct{}{}, nil
+}
+
+var benchLookupHelper = helpers.Register("benchLookup", httpjson.ServePOST(doBenchLookup))
+
 func BenchmarkLookup(b *testing.B) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	f := &benchLookupDir{}
 	mnt, err := fstestutil.MountedT(b, fstestutil.SimpleFS{f}, nil)
 	if err != nil {
 		b.Fatal(err)
 	}
 	defer mnt.Close()
+	control := benchLookupHelper.Spawn(ctx, b)
+	defer control.Close()
 
 	name := mnt.Dir + "/does-not-exist"
+	req := benchLookupRequest{
+		Path: name,
+		N:    b.N,
+	}
+	var nothing struct{}
 	b.ResetTimer()
 
-	for i := 0; i < b.N; i++ {
-		if _, err := os.Stat(name); !os.IsNotExist(err) {
-			b.Fatalf("Stat: wrong error: %v", err)
-		}
+	if err := control.JSON("/").Call(ctx, req, &nothing); err != nil {
+		b.Fatalf("calling helper: %v", err)
 	}
 }
