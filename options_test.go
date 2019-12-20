abd88a2169b7104a5b42a67bb3138a072b268704
Tommi Virtanen <tv@eagain.net>
2019-12-19 20:22:47 -0700

    Use spawntest for TestMountOptionDefaultPermissions

--- options_test.go
+++ options_test.go
@@ -2,6 +2,8 @@ package fuse_test
 
 import (
 	"context"
+	"errors"
+	"flag"
 	"os"
 	"runtime"
 	"syscall"
@@ -10,6 +12,8 @@ import (
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
 	"bazil.org/fuse/fs/fstestutil"
+	"bazil.org/fuse/fs/fstestutil/spawntest"
+	"bazil.org/fuse/fs/fstestutil/spawntest/httpjson"
 )
 
 func init() {
@@ -18,6 +22,15 @@ func init() {
 
 func maybeParallel(t *testing.T) {
 	// t.Parallel()
+}
+
+var helpers spawntest.Registry
+
+func TestMain(m *testing.M) {
+	helpers.AddFlag(flag.CommandLine)
+	flag.Parse()
+	helpers.RunIfNeeded()
+	os.Exit(m.Run())
 }
 
 func TestMountOptionFSName(t *testing.T) {
@@ -164,11 +177,33 @@ func (f unwritableFile) Attr(ctx context.Context, a *fuse.Attr) error {
 	return nil
 }
 
+type openRequest struct {
+	Path      string
+	Flags     int
+	Perm      os.FileMode
+	WantErrno syscall.Errno
+}
+
+func doOpenErr(ctx context.Context, req openRequest) (*struct{}, error) {
+	f, err := os.OpenFile(req.Path, req.Flags, req.Perm)
+	if err == nil {
+		f.Close()
+	}
+	if !errors.Is(err, req.WantErrno) {
+		return nil, err
+	}
+	return &struct{}{}, nil
+}
+
+var openErrHelper = helpers.Register("openErr", httpjson.ServePOST(doOpenErr))
+
 func TestMountOptionDefaultPermissions(t *testing.T) {
 	if runtime.GOOS == "freebsd" {
 		t.Skip("FreeBSD does not support DefaultPermissions")
 	}
 	maybeParallel(t)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	mnt, err := fstestutil.MountedT(t,
 		fstestutil.SimpleFS{
@@ -181,16 +216,20 @@ func TestMountOptionDefaultPermissions(t *testing.T) {
 		t.Fatal(err)
 	}
 	defer mnt.Close()
+	control := openErrHelper.Spawn(ctx, t)
+	defer control.Close()
 
 	// This will be prevented by kernel-level access checking when
 	// DefaultPermissions is used.
-	f, err := os.OpenFile(mnt.Dir+"/child", os.O_WRONLY, 0000)
-	if err == nil {
-		f.Close()
-		t.Fatal("expected an error")
+	req := openRequest{
+		Path:      mnt.Dir + "/child",
+		Flags:     os.O_WRONLY,
+		Perm:      0,
+		WantErrno: syscall.EACCES,
 	}
-	if !os.IsPermission(err) {
-		t.Fatalf("expected a permission error, got %T: %v", err, err)
+	var nothing struct{}
+	if err := control.JSON("/").Call(ctx, req, &nothing); err != nil {
+		t.Fatalf("calling helper: %v", err)
 	}
 }
 
