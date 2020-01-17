b43d6a229c276d1a7083d493a7b6e25204607bd4
Tommi Virtanen <tv@eagain.net>
2020-01-17 15:18:06 -0700

    Silence staticcheck false positive

--- options_helper_test.go
+++ options_helper_test.go
@@ -1,5 +1,7 @@
 package fuse
 
+//lint:ignore U1000 Used by TestMountOptionCommaError to bypass safe API
+
 // for TestMountOptionCommaError
 func ForTestSetMountOption(k, v string) MountOption {
 	fn := func(conf *mountConfig) error {
