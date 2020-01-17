1b878a0067e6ef10466858e65cb252c52c0be00d
Tommi Virtanen <tv@eagain.net>
2020-01-17 15:24:16 -0700

    Move cstr helper back to darwin only
    FreeBSD ended up not having anything useful where we expected to need
    it.

--- fs/fstestutil/mountinfo_darwin.go
+++ fs/fstestutil/mountinfo_darwin.go
@@ -13,6 +13,18 @@ func unescape(s string) string {
 	return re.ReplaceAllString(s, `$1`)
 }
 
+// cstr converts a nil-terminated C string into a Go string
+func cstr(ca []int8) string {
+	s := make([]byte, 0, len(ca))
+	for _, c := range ca {
+		if c == 0x00 {
+			break
+		}
+		s = append(s, byte(c))
+	}
+	return string(s)
+}
+
 func getMountInfo(mnt string) (*MountInfo, error) {
 	var st syscall.Statfs_t
 	err := syscall.Statfs(mnt, &st)
